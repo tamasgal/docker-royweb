fe9c78f2d1a17a4a275515bf903b144c5965396d
Tamas Gal <himself@tamasgal.com>
2017-09-10 23:54:01 +0200

    Add server specific configuration

--- setup.sh
+++ setup.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
+set -e
 
 # build the image
 echo "Building the Docker image"
 docker build -t royweb .
 
 # startup
-docker run -it \
+docker run -d \
     -p 8080:8080 \
     -p 9999:9999/udp \
     -v $(pwd)/sessions:/sessions \
     --name royweb \
-    royweb
+    royweb \
+    royweb --ip=193.48.106.31
 
 echo "The royweb service is now up and running."
